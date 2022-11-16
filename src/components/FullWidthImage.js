32ffb8a19793e03cd2c8b15e12e7d808e668380e
ljpernic <leonXperniciaro@gmail.com>
2022-11-15 21:58:51 -0500

    styling front page

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -69,8 +69,8 @@ export default function FullWidthImage(props) {
                 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
                 style={{
                   boxShadow:
-                    "$primary 0.5rem 0px 0px, #primary -0.5rem 0px 0px",
-                  backgroundColor: $primary,
+                    "rgb(43, 37, 35) 0.5rem 0px 0px, rgb(43, 37, 35) -0.5rem 0px 0px",
+                  backgroundColor: "rgb(43, 37, 35)",
                   color: "white",
                   lineHeight: "1",
                   padding: "0.25em",
@@ -84,8 +84,8 @@ export default function FullWidthImage(props) {
                 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
                 style={{
                   boxShadow:
-                    "$primary 0.5rem 0px 0px, #primary -0.5rem 0px 0px",
-                  backgroundColor: $primary,
+                    "rgb(43, 37, 35) 0.5rem 0px 0px, rgb(43, 37, 35) -0.5rem 0px 0px",
+                  backgroundColor: "rgb(43, 37, 35)",
                   color: "white",
                   lineHeight: "1",
                   padding: "0.25rem",
