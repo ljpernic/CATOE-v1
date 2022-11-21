3c0b495c818ba18e701ca23dc850b8dd176a339e
ljpernic <leonXperniciaro@gmail.com>
2022-11-20 21:07:04 -0500

    white space front page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Ho Minti Society",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "The Ho Minti Society is a community organization in support of the Choctaw-Apache Tribe of Ebarb",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
